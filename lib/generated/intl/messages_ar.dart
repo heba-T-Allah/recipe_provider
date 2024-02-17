6915b4dc3d03305ef6f75685f681eaf607788373
heba <beboozain7@gmail.com>
2024-02-17 02:40:07 +0200

    add arabic language and back button to exit and to go to home

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,132 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Areyousureyouwanttoexit": MessageLookupByLibrary.simpleMessage(
+            "هل تريد بالفعل الخروج من البرنامج؟"),
+        "Bonjour": MessageLookupByLibrary.simpleMessage("مرحبا"),
+        "Breakfast": MessageLookupByLibrary.simpleMessage("فطور"),
+        "Dinner": MessageLookupByLibrary.simpleMessage("عشاء"),
+        "ExitApp": MessageLookupByLibrary.simpleMessage("خروج"),
+        "Launch": MessageLookupByLibrary.simpleMessage("غذاء"),
+        "No": MessageLookupByLibrary.simpleMessage("إلغاء"),
+        "NoDataFound": MessageLookupByLibrary.simpleMessage("لا يوجد نتائج"),
+        "NoFilterDataEntered":
+            MessageLookupByLibrary.simpleMessage("لم يتم ادخال بيانات ."),
+        "Step": MessageLookupByLibrary.simpleMessage("خطوه"),
+        "Yes": MessageLookupByLibrary.simpleMessage("تأكيد"),
+        "YouLogedinSuccessfully":
+            MessageLookupByLibrary.simpleMessage("تم الدخول بنجاح"),
+        "aboutUs": MessageLookupByLibrary.simpleMessage("عن التطبيق"),
+        "aboutUsContent": MessageLookupByLibrary.simpleMessage(
+            "تطبيق وصفة هذا مخصص للأشخاص الذين يحتارون في اختيار الوجبات، حيث يتيح لهم إضافة المكونات المتوفرة في مطبخهم واختيار الوجبة المناسبة لهم. ويمكنهم أيضًا عرض الوجبات التي شاهدوها مؤخرًا، وإضافة الوجبات إلى المفضلة، وإزالتهم."),
+        "allRecipe": MessageLookupByLibrary.simpleMessage("كل الوصفات"),
+        "alreadyRegistered":
+            MessageLookupByLibrary.simpleMessage("هل لديك حساب بالفعل؟"),
+        "appTitle": MessageLookupByLibrary.simpleMessage("تطبيق وصفة"),
+        "apply": MessageLookupByLibrary.simpleMessage("تطبيق"),
+        "arabic": MessageLookupByLibrary.simpleMessage("العربيه"),
+        "calorie": MessageLookupByLibrary.simpleMessage("سعر حراري"),
+        "calories": MessageLookupByLibrary.simpleMessage("سعرات حرارية"),
+        "changeName": MessageLookupByLibrary.simpleMessage("تغيير الإسم"),
+        "confirmPassword":
+            MessageLookupByLibrary.simpleMessage("تأكيد كلمة المرور"),
+        "directions": MessageLookupByLibrary.simpleMessage("الخطوات"),
+        "donotHaveAccount":
+            MessageLookupByLibrary.simpleMessage("ليس لديك حساب؟ "),
+        "emailHintText":
+            MessageLookupByLibrary.simpleMessage("البريد الإلكتروني"),
+        "emailIsEmpty":
+            MessageLookupByLibrary.simpleMessage("برجاء إدخال الايميل"),
+        "emailPref": MessageLookupByLibrary.simpleMessage("email"),
+        "english": MessageLookupByLibrary.simpleMessage("English"),
+        "errorWhenGetData": MessageLookupByLibrary.simpleMessage("حدث خطأ"),
+        "favorites": MessageLookupByLibrary.simpleMessage("المفضله"),
+        "fieldIsEmpty": MessageLookupByLibrary.simpleMessage("الحقل فارغ"),
+        "filter": MessageLookupByLibrary.simpleMessage("تصفيه"),
+        "forgetPassword":
+            MessageLookupByLibrary.simpleMessage("نسيت كلمة المرور"),
+        "fullNameHintText":
+            MessageLookupByLibrary.simpleMessage("الاسم الكامل"),
+        "home": MessageLookupByLibrary.simpleMessage("الرئيسيه"),
+        "homeScreen": MessageLookupByLibrary.simpleMessage("الرئيسيه"),
+        "ingredients": MessageLookupByLibrary.simpleMessage("المقادير"),
+        "isLoggedInPref": MessageLookupByLibrary.simpleMessage("loggedIn"),
+        "language": MessageLookupByLibrary.simpleMessage("اللغه"),
+        "logOut": MessageLookupByLibrary.simpleMessage("خروج"),
+        "login": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+        "mailSentSuccessfully":
+            MessageLookupByLibrary.simpleMessage("تم ارسال الايميل"),
+        "meal": MessageLookupByLibrary.simpleMessage("وجبة"),
+        "min": MessageLookupByLibrary.simpleMessage("دقيقه"),
+        "nameIsEmpty":
+            MessageLookupByLibrary.simpleMessage("برجاء إدخال الإسم."),
+        "notRegistered":
+            MessageLookupByLibrary.simpleMessage("Not registered yet,Sign Up"),
+        "notValidEmail":
+            MessageLookupByLibrary.simpleMessage("بريد إلكتروني غير صالح"),
+        "password": MessageLookupByLibrary.simpleMessage("كلمه المرور"),
+        "passwordIsEmpty":
+            MessageLookupByLibrary.simpleMessage("برجاء إدخال كلمه السر."),
+        "passwordPref": MessageLookupByLibrary.simpleMessage("password"),
+        "passwordTooShort":
+            MessageLookupByLibrary.simpleMessage("كلمة المرور قصيرة جدا"),
+        "pleaseEnterValidPassword": MessageLookupByLibrary.simpleMessage(
+            "الرجاء إدخال كلمة السر الصحيحة"),
+        "pleaseEnterYourName":
+            MessageLookupByLibrary.simpleMessage("من فضلك ادخل الاسم"),
+        "preparationTime": MessageLookupByLibrary.simpleMessage("وقت التحضير"),
+        "profile": MessageLookupByLibrary.simpleMessage("الملف الشخصي"),
+        "rate": MessageLookupByLibrary.simpleMessage("تقييم"),
+        "recentlyViewed": MessageLookupByLibrary.simpleMessage("شوهدت مؤخرا"),
+        "recommended": MessageLookupByLibrary.simpleMessage("موصى به"),
+        "register": MessageLookupByLibrary.simpleMessage(" اشتراك"),
+        "registerDot": MessageLookupByLibrary.simpleMessage("تسجيل ."),
+        "reset": MessageLookupByLibrary.simpleMessage("إعادة ضبط"),
+        "search": MessageLookupByLibrary.simpleMessage("بحث"),
+        "searchUsingKeyword":
+            MessageLookupByLibrary.simpleMessage("البحث باستخدام الاسم "),
+        "seeAll": MessageLookupByLibrary.simpleMessage("عرض الكل"),
+        "sendMail": MessageLookupByLibrary.simpleMessage("ارسل بريد"),
+        "serving": MessageLookupByLibrary.simpleMessage("عدد الوجبات"),
+        "servingNum": MessageLookupByLibrary.simpleMessage("وجبه"),
+        "setting": MessageLookupByLibrary.simpleMessage("الضبط"),
+        "signIn": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+        "signOut": MessageLookupByLibrary.simpleMessage("تسجيل خروج"),
+        "signUp": MessageLookupByLibrary.simpleMessage("اشتراك"),
+        "somethingWentWrong": MessageLookupByLibrary.simpleMessage(
+            "حدث خطأ يرجى المحاوله لاحقا."),
+        "splashTitle":
+            MessageLookupByLibrary.simpleMessage("الطبخ بطريقة سهلة"),
+        "thereIsNoData": MessageLookupByLibrary.simpleMessage("لا يوجد بيانات"),
+        "todayFresh": MessageLookupByLibrary.simpleMessage("وصفات اليوم"),
+        "updateProfile":
+            MessageLookupByLibrary.simpleMessage("تحديث الملف الشخصي"),
+        "userName": MessageLookupByLibrary.simpleMessage("اسم المستخدم"),
+        "viewProfile": MessageLookupByLibrary.simpleMessage("الملف الشخصي"),
+        "whatWouldYouLikeToCook": MessageLookupByLibrary.simpleMessage(
+            "ما الذي ترغب في طهيه \n اليوم؟"),
+        "youRegisteredSuccessfully":
+            MessageLookupByLibrary.simpleMessage("تم التسجيل بنجاح")
+      };
+}
---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,132 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Areyousureyouwanttoexit": MessageLookupByLibrary.simpleMessage(
+            "Are you sure you want to exit?"),
+        "Bonjour": MessageLookupByLibrary.simpleMessage("Bonjour"),
+        "Breakfast": MessageLookupByLibrary.simpleMessage("Breakfast"),
+        "Dinner": MessageLookupByLibrary.simpleMessage("Dinner"),
+        "ExitApp": MessageLookupByLibrary.simpleMessage("Exit App"),
+        "Launch": MessageLookupByLibrary.simpleMessage("Launch"),
+        "No": MessageLookupByLibrary.simpleMessage("No"),
+        "NoDataFound": MessageLookupByLibrary.simpleMessage("No Data Found"),
+        "NoFilterDataEntered":
+            MessageLookupByLibrary.simpleMessage("No filter Data entered."),
+        "Step": MessageLookupByLibrary.simpleMessage("Step"),
+        "Yes": MessageLookupByLibrary.simpleMessage("Yes"),
+        "YouLogedinSuccessfully":
+            MessageLookupByLibrary.simpleMessage("You login Successfully"),
+        "aboutUs": MessageLookupByLibrary.simpleMessage("About Us"),
+        "aboutUsContent": MessageLookupByLibrary.simpleMessage(
+            "this Recipe app is for people who are confused about choosing meals, as it allows them to add the ingredients available in their kitchen and choose the appropriate meal for them. they can also view the meals they have recently viewed, add meals to favorites, and remove them."),
+        "allRecipe": MessageLookupByLibrary.simpleMessage("All Recipe"),
+        "alreadyRegistered":
+            MessageLookupByLibrary.simpleMessage("Already have account?"),
+        "appTitle": MessageLookupByLibrary.simpleMessage("Recipe App"),
+        "apply": MessageLookupByLibrary.simpleMessage("Apply"),
+        "arabic": MessageLookupByLibrary.simpleMessage("العربيه"),
+        "calorie": MessageLookupByLibrary.simpleMessage("Calories"),
+        "calories": MessageLookupByLibrary.simpleMessage("Calories"),
+        "changeName": MessageLookupByLibrary.simpleMessage("Change Name"),
+        "confirmPassword":
+            MessageLookupByLibrary.simpleMessage("Confirm Password"),
+        "directions": MessageLookupByLibrary.simpleMessage("Directions"),
+        "donotHaveAccount":
+            MessageLookupByLibrary.simpleMessage("Don\'t have an account? "),
+        "emailHintText": MessageLookupByLibrary.simpleMessage("Email Address"),
+        "emailIsEmpty": MessageLookupByLibrary.simpleMessage("Email is empty!"),
+        "emailPref": MessageLookupByLibrary.simpleMessage("email"),
+        "english": MessageLookupByLibrary.simpleMessage("English"),
+        "errorWhenGetData":
+            MessageLookupByLibrary.simpleMessage("Error when get data"),
+        "favorites": MessageLookupByLibrary.simpleMessage("Favorites"),
+        "fieldIsEmpty": MessageLookupByLibrary.simpleMessage("field is empty"),
+        "filter": MessageLookupByLibrary.simpleMessage("Filter"),
+        "forgetPassword":
+            MessageLookupByLibrary.simpleMessage("Forget Password"),
+        "fullNameHintText": MessageLookupByLibrary.simpleMessage("Full Name"),
+        "home": MessageLookupByLibrary.simpleMessage("Home"),
+        "homeScreen": MessageLookupByLibrary.simpleMessage("Home"),
+        "ingredients": MessageLookupByLibrary.simpleMessage("Ingredients"),
+        "isLoggedInPref": MessageLookupByLibrary.simpleMessage("loggedIn"),
+        "language": MessageLookupByLibrary.simpleMessage("Language"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "login": MessageLookupByLibrary.simpleMessage("Login"),
+        "mailSentSuccessfully":
+            MessageLookupByLibrary.simpleMessage("Mail sent Successfully"),
+        "meal": MessageLookupByLibrary.simpleMessage("Meal"),
+        "min": MessageLookupByLibrary.simpleMessage("Min"),
+        "nameIsEmpty": MessageLookupByLibrary.simpleMessage("name is empty!"),
+        "notRegistered":
+            MessageLookupByLibrary.simpleMessage("Not registered yet,Sign Up"),
+        "notValidEmail":
+            MessageLookupByLibrary.simpleMessage("Not Valid Email"),
+        "password": MessageLookupByLibrary.simpleMessage("Password"),
+        "passwordIsEmpty":
+            MessageLookupByLibrary.simpleMessage("password is empty!"),
+        "passwordPref": MessageLookupByLibrary.simpleMessage("password"),
+        "passwordTooShort":
+            MessageLookupByLibrary.simpleMessage("Password is too short"),
+        "pleaseEnterValidPassword": MessageLookupByLibrary.simpleMessage(
+            "Please enter a valid Password"),
+        "pleaseEnterYourName":
+            MessageLookupByLibrary.simpleMessage("Please enter your name."),
+        "preparationTime":
+            MessageLookupByLibrary.simpleMessage("Preparation Time"),
+        "profile": MessageLookupByLibrary.simpleMessage("Profile"),
+        "rate": MessageLookupByLibrary.simpleMessage("Rating"),
+        "recentlyViewed":
+            MessageLookupByLibrary.simpleMessage("RecentlyViewed"),
+        "recommended": MessageLookupByLibrary.simpleMessage("Recommended"),
+        "register": MessageLookupByLibrary.simpleMessage(" Register"),
+        "registerDot": MessageLookupByLibrary.simpleMessage("Register."),
+        "reset": MessageLookupByLibrary.simpleMessage("Reset"),
+        "search": MessageLookupByLibrary.simpleMessage("Search"),
+        "searchUsingKeyword":
+            MessageLookupByLibrary.simpleMessage("Search using keyword"),
+        "seeAll": MessageLookupByLibrary.simpleMessage("See All"),
+        "sendMail": MessageLookupByLibrary.simpleMessage("Send Mail"),
+        "serving": MessageLookupByLibrary.simpleMessage("Serving"),
+        "servingNum": MessageLookupByLibrary.simpleMessage("Serving"),
+        "setting": MessageLookupByLibrary.simpleMessage("Setting"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Sign In"),
+        "signOut": MessageLookupByLibrary.simpleMessage("Sign Out"),
+        "signUp": MessageLookupByLibrary.simpleMessage("Sign Up"),
+        "somethingWentWrong": MessageLookupByLibrary.simpleMessage(
+            "Something went wrong Please try again"),
+        "splashTitle":
+            MessageLookupByLibrary.simpleMessage("Cooking Done The easy way"),
+        "thereIsNoData":
+            MessageLookupByLibrary.simpleMessage("There is no data"),
+        "todayFresh":
+            MessageLookupByLibrary.simpleMessage("Today \'s Fresh Recipes"),
+        "updateProfile": MessageLookupByLibrary.simpleMessage("Update Profile"),
+        "userName": MessageLookupByLibrary.simpleMessage("User name"),
+        "viewProfile": MessageLookupByLibrary.simpleMessage("View Profile"),
+        "whatWouldYouLikeToCook": MessageLookupByLibrary.simpleMessage(
+            "What Would you Like to Cook \n Today?"),
+        "youRegisteredSuccessfully":
+            MessageLookupByLibrary.simpleMessage("You registered Successfully")
+      };
+}
